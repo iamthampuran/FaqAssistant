5a2b590a347725dfb60731fb6a4f134d02c5e451
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-12-19 12:54:24 +0530

    Query methods added

--- FaqAssistant.Application/Interfaces/Repositories/ICategoryRepository.cs
+++ FaqAssistant.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -1,7 +1,16 @@
+using FaqAssistant.Application.Common;
+using FaqAssistant.Application.Features.Category.Queries.GetCategoryDetails;
+using FaqAssistant.Application.Features.Tag.Queries.GetTagDetails;
 using FaqAssistant.Domain.Entities;
 
 namespace FaqAssistant.Application.Interfaces.Repositories;
 
 public interface ICategoryRepository : IGenericRepository<Category>
 {
+    public Task<PagedResult<GetCategoryDetailsResponse>> GetCategoryDetailsAsync(
+    int pageNumber,
+    int pageSize,
+    string? searchValue,
+    CancellationToken cancellationToken);
+
 }
