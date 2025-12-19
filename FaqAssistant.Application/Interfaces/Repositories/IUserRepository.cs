a7c2332a802eefbce150b63c39ad24a6bc308731
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-12-19 11:09:47 +0530

    Modified userId response

--- FaqAssistant.Application/Interfaces/Repositories/IUserRepository.cs
+++ FaqAssistant.Application/Interfaces/Repositories/IUserRepository.cs
@@ -7,7 +7,7 @@ namespace FaqAssistant.Application.Interfaces.Repositories;
 
 public interface IUserRepository : IGenericRepository<User>
 {
-    public Task<GetUserByIdResponse?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
+    public Task<GetUserDetailsByIdQueryResponse?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
     public Task<PagedResult<GetUserDetailsQueryResponse>> GetUserDetailsAsync(int pageNumber, int pageSize, string? searchValue, CancellationToken cancellationToken = default);
 
 }
