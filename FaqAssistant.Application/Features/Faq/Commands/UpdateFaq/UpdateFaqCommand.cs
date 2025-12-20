c639e2776937d7cabb5c05e3e465aedb1c452beb
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-12-20 08:37:23 +0530

    Fixed the namespace format

--- FaqAssistant.Application/Features/Faq/Commands/UpdateFaq/UpdateFaqCommand.cs
+++ FaqAssistant.Application/Features/Faq/Commands/UpdateFaq/UpdateFaqCommand.cs
@@ -1,16 +1,15 @@
 ﻿using FaqAssistant.Application.Helpers;
 using MediatR;
 
-namespace FaqAssistant.Application.Features.Faq.Commands.UpdateFaq
+namespace FaqAssistant.Application.Features.Faq.Commands.UpdateFaq;
+
+public class UpdateFaqCommand : IRequest<Result<Guid>>
 {
-    public class UpdateFaqCommand : IRequest<Result<Guid>>
-    {
-        public Guid Id { get; set; }
-        public string Question { get; set; } = null!;
-        public string Answer { get; set; } = null!;
-        public Guid UserId { get; set; }
-        public Guid CategoryId { get; set; }
-        public List<Guid> TagIds { get; set; } = [];
-    
-    }
+    public Guid Id { get; set; }
+    public string Question { get; set; } = null!;
+    public string Answer { get; set; } = null!;
+    public Guid UserId { get; set; }
+    public Guid CategoryId { get; set; }
+    public List<Guid> TagIds { get; set; } = [];
+
 }
--- FaqAssistant.Infrastructure/Migrations/20251219185307_InitialCreate.cs
+++ FaqAssistant.Infrastructure/Migrations/20251219185307_InitialCreate.cs
@@ -3,158 +3,157 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace FaqAssistant.Infrastructure.Migrations
+namespace FaqAssistant.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Categories",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Name = table.Column<string>(type: "text", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Categories", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Categories",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                Name = table.Column<string>(type: "text", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Categories", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Tags",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Name = table.Column<string>(type: "text", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Tags", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Tags",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                Name = table.Column<string>(type: "text", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Tags", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Username = table.Column<string>(type: "text", nullable: false),
-                    Email = table.Column<string>(type: "text", nullable: false),
-                    PasswordHash = table.Column<string>(type: "text", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Users",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                Username = table.Column<string>(type: "text", nullable: false),
+                Email = table.Column<string>(type: "text", nullable: false),
+                PasswordHash = table.Column<string>(type: "text", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Users", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Faqs",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Question = table.Column<string>(type: "text", nullable: false),
-                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
-                    Answer = table.Column<string>(type: "text", nullable: false),
-                    Rating = table.Column<int>(type: "integer", nullable: false),
-                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Faqs", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Faqs_Categories_CategoryId",
-                        column: x => x.CategoryId,
-                        principalTable: "Categories",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Faqs_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Faqs",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                Question = table.Column<string>(type: "text", nullable: false),
+                CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                Answer = table.Column<string>(type: "text", nullable: false),
+                Rating = table.Column<int>(type: "integer", nullable: false),
+                UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Faqs", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Faqs_Categories_CategoryId",
+                    column: x => x.CategoryId,
+                    principalTable: "Categories",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Faqs_Users_UserId",
+                    column: x => x.UserId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "FaqTag",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    FaqId = table.Column<Guid>(type: "uuid", nullable: false),
-                    TagId = table.Column<Guid>(type: "uuid", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FaqTag", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_FaqTag_Faqs_FaqId",
-                        column: x => x.FaqId,
-                        principalTable: "Faqs",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_FaqTag_Tags_TagId",
-                        column: x => x.TagId,
-                        principalTable: "Tags",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
+        migrationBuilder.CreateTable(
+            name: "FaqTag",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                FaqId = table.Column<Guid>(type: "uuid", nullable: false),
+                TagId = table.Column<Guid>(type: "uuid", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                LastUpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_FaqTag", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_FaqTag_Faqs_FaqId",
+                    column: x => x.FaqId,
+                    principalTable: "Faqs",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Restrict);
+                table.ForeignKey(
+                    name: "FK_FaqTag_Tags_TagId",
+                    column: x => x.TagId,
+                    principalTable: "Tags",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Restrict);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Faqs_CategoryId",
-                table: "Faqs",
-                column: "CategoryId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Faqs_CategoryId",
+            table: "Faqs",
+            column: "CategoryId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Faqs_UserId",
-                table: "Faqs",
-                column: "UserId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Faqs_UserId",
+            table: "Faqs",
+            column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_FaqTag_FaqId_TagId",
-                table: "FaqTag",
-                columns: new[] { "FaqId", "TagId" });
+        migrationBuilder.CreateIndex(
+            name: "IX_FaqTag_FaqId_TagId",
+            table: "FaqTag",
+            columns: new[] { "FaqId", "TagId" });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_FaqTag_TagId",
-                table: "FaqTag",
-                column: "TagId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_FaqTag_TagId",
+            table: "FaqTag",
+            column: "TagId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "FaqTag");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "FaqTag");
 
-            migrationBuilder.DropTable(
-                name: "Faqs");
+        migrationBuilder.DropTable(
+            name: "Faqs");
 
-            migrationBuilder.DropTable(
-                name: "Tags");
+        migrationBuilder.DropTable(
+            name: "Tags");
 
-            migrationBuilder.DropTable(
-                name: "Categories");
+        migrationBuilder.DropTable(
+            name: "Categories");
 
-            migrationBuilder.DropTable(
-                name: "Users");
-        }
+        migrationBuilder.DropTable(
+            name: "Users");
     }
 }
